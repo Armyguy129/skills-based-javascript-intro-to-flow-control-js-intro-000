841871ccdd4974625154db98d3516a6c6a746304
Steven Gomez <gomezs0725@gmail.com>
2020-05-26 00:48:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,13 +2,13 @@ function basicTeenager(age) {if (age >= 13 && age <=19)
 return "You are a teenager!";
 }
 
-function teenager(age) {if (age >=13 && age <= 19) return "You are a teenager!"; else { return "You are not a teenager!"; 
+function teenager(age) {if (age >=13 && age <= 19) return "You are a teenager!"; else return "You are not a teenager!"; 
 }
 
 function ageChecker(age) { if (age <= 12) return "you are a kid!"; else if ( age >= 13 && age <= 19) return "You are a teenager!"; else return "You are a grownup!";
 }
 
-function ternaryTeenager(age) { return age >= 13 && age >= 19; return "You are a teenager!"; else if 
+function ternaryTeenager(age) { if (age >= 13 && age <= 19); return "You are a teenager!";
 
 }
 
